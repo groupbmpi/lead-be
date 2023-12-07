6f1fbf91ee7e6522bc2cb81180f7f6a2a8c521ef
Nuzul Abatony <albtony@gmail.com>
2023-12-07 14:54:20 +0700

    fix: model creates new table instead using default table

--- models/informationBanner.js
+++ models/informationBanner.js
@@ -2,24 +2,29 @@ const { DataTypes } = require('sequelize');
 const { db } = require('../config/db');
 
 const InformationBanner = db.define('InformationBanner', {
-    information_banner_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-      validate: {
-        isInt: true,
-        notEmpty: true,
-      },
+  information_banner_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      notEmpty: true,
     },
-    url_picture: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    text: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-  });
-  
+  },
+  url_picture: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+}, {
+  sequelize: db,
+  modelName: 'InformationBanner',
+  tableName: 'information_banner', 
+  timestamps: false
+});
+
 module.exports = InformationBanner;
