0652b4f7f2b1212965a7f5a0199378387a56ac21
Nuzul Abatony <albtony@gmail.com>
2023-12-07 22:24:28 +0700

    fix: some syntax errors

--- models/mentor.js
+++ models/mentor.js
@@ -1,69 +1,71 @@
 const { Model, DataTypes } = require('sequelize');
-const db = require('../config/db');
-const Mentor = db.define('Mentor', {
-    mentor_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmpty: false,
-      },
-    },
-    category: {
-      type: DataTypes.ENUM('Desain Program', 'Cluster'),
-      allowNull: false
-    },
-    birthdate: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    gender: {
-      type: DataTypes.ENUM('Pria', 'Wanita'),
-      allowNull: true
-    },
-    phone_number: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    education_background: {
-      type: DataTypes.ENUM('S1', 'S2', 'S3'),
-      allowNull: true
-    },
-    position: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    current_workplace: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    url_picture: {
-      type: DataTypes.STRING,
-      allowNull: true
-    }, 
-    role: {
-      type: DataTypes.ENUM('MENTOR'), // untuk JWT claim token
-      defaultValue: 'MENTOR',
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Mentor',
-    tableName: 'mentors',
-    underscored: true,
-    timestamps: false
-  });
+const { db } = require('../config/db');
 
-  export default Mentor;
+const Mentor = db.define('Mentor', {
+  mentor_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  category: {
+    type: DataTypes.ENUM('Desain Program', 'Cluster'),
+    allowNull: false,
+  },
+  birthdate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  gender: {
+    type: DataTypes.ENUM('Pria', 'Wanita'),
+    allowNull: true,
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  education_background: {
+    type: DataTypes.ENUM('S1', 'S2', 'S3'),
+    allowNull: true,
+  },
+  position: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  current_workplace: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  url_picture: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  role: {
+    type: DataTypes.ENUM('MENTOR'),
+    defaultValue: 'MENTOR',
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Mentor',
+  tableName: 'mentors',
+  underscored: true,
+  timestamps: false,
+});
+
+module.exports = Mentor;
