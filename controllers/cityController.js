2f852a7a188f67830e0b37425a05c4d3cede3016
M. Fadhil Mahendra <mfmahendr@gmail.com>
2023-12-10 15:11:40 +0700

    docs: add province route & city route documentation

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,7 +1,7 @@
 const City = require('../models/city');
+const Province = require('../models/province');
 const { successResponse, errorResponse } = require('../utils/responseBuilder');
 
-ProvinceController = require('./provinceController');
 
 // Get all cities
 const getAllCities = async (req, res) => {
@@ -36,68 +36,68 @@ const getCityById = async (req, res) => {
     }
 };
 
-// Create a new city
-const createCity = async (req, res) => {
-    const { name, province_id } = req.body;
-    try {
-        const province = await ProvinceController.getProvinceById(province_id);
-        if (!province) res.status(404).json(errorResponse(404, 'Province not found'));
+// // Create a new city
+// const createCity = async (req, res) => {
+//     const { name, province_id } = req.body;
+//     try {
+//         const province = await Province.findByPk(province_id);
+//         if (!province) res.status(404).json(errorResponse(404, 'Province not found'));
         
-        const city = await City.create({ name, province_id });
-        res.status(201).json(successResponse(201, 'City created', {
-            id: city.id,
-            name: city.name,
-            province_id: city.province_id,
-        }));
-    } catch (error) {
-        res.status(500).json(errorResponse(error.code, error.message));
-    }
-};
+//         const city = await City.create({ name, province_id });
+//         res.status(201).json(successResponse(201, 'City created', {
+//             id: city.id,
+//             name: city.name,
+//             province_id: city.province_id,
+//         }));
+//     } catch (error) {
+//         res.status(500).json(errorResponse(error.code, error.message));
+//     }
+// };
 
-// Update a city
-const updateCity = async (req, res) => {
-    const { id } = req.params;
-    const { name, province_id } = req.body;
-    try {
-        const city = await City.findByPk(id);
-        if (!city) {
-            res.status(404).json(errorResponse(404, 'City not found'));
-        }
+// // Update a city
+// const updateCity = async (req, res) => {
+//     const { id } = req.params;
+//     const { name, province_id } = req.body;
+//     try {
+//         const city = await City.findByPk(id);
+//         if (!city) {
+//             res.status(404).json(errorResponse(404, 'City not found'));
+//         }
 
-        city.name = name;
-        city.province_id = province_id;
-        await city.save();
+//         city.name = name;
+//         city.province_id = province_id;
+//         await city.save();
 
-        res.json(successResponse(200, 'City updated', {
-            id: city.id,
-            name: city.name,
-            province_id: city.province_id,
-        }));
-    } catch (error) {
-        res.status(500).json(errorResponse(error.code, error.message));
-    }
-};
+//         res.json(successResponse(200, 'City updated', {
+//             id: city.id,
+//             name: city.name,
+//             province_id: city.province_id,
+//         }));
+//     } catch (error) {
+//         res.status(500).json(errorResponse(error.code, error.message));
+//     }
+// };
 
-// Delete a city
-const deleteCity = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const city = await City.findByPk(id);
-        if (city) {
-            await city.destroy();
-            res.json(successResponse(200, 'City deleted'));
-        } else {
-            res.status(404).json(errorResponse(404, 'City not found'));
-        }
-    } catch (error) {
-        res.status(500).json(errorResponse(error.code, error.message));
-    }
-};
+// // Delete a city
+// const deleteCity = async (req, res) => {
+//     const { id } = req.params;
+//     try {
+//         const city = await City.findByPk(id);
+//         if (city) {
+//             await city.destroy();
+//             res.json(successResponse(200, 'City deleted'));
+//         } else {
+//             res.status(404).json(errorResponse(404, 'City not found'));
+//         }
+//     } catch (error) {
+//         res.status(500).json(errorResponse(error.code, error.message));
+//     }
+// };
 
 module.exports = {
     getAllCities,
     getCityById,
-    createCity,
-    updateCity,
-    deleteCity,
+    // createCity,
+    // updateCity,
+    // deleteCity,
 };
--- routes/provinceRoute.js
+++ routes/provinceRoute.js
@@ -13,9 +13,161 @@ const {
     // deleteProvinceById
 } = require('../controllers/provinceController');
 
+/**
+ * @swagger
+ * tags:
+ *  name: Province
+ *  description: Province management API
+ */
+
+
+/**
+ * @swagger
+ * /api/v1/province:
+ *   get:
+ *     summary: Get all provinces
+ *     description: Retrieve a list of all provinces
+ *     tags: [Province]
+ *     responses:
+ *       200:
+ *         description: Provinces retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: number
+ *                   example: 200
+ *                   description: The HTTP status code indicating success.
+ *                 status:
+ *                   type: string
+ *                   example: success
+ *                   description: The status of the response.
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                   description: A brief message describing the result.
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: number
+ *                         example: 1
+ *                         description: The unique identifier of the province.
+ *                       name:
+ *                         type: string
+ *                         example: Jakarta
+ *                         description: The name of the province.
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: number
+ *                   example: 500
+ *                   description: The HTTP status code indicating an internal server error.
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                   description: The status of the response indicating an error.
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ *                   description: A brief message describing the error.
+ */
 // Get all provinces
 router.get('/api/v1/province', getAllProvinces);
 
+/**
+ * @swagger
+ * /api/v1/province/{id}:
+ *   get:
+ *     summary: Get a province by ID
+ *     description: Retrieve information about a province based on its unique identifier.
+ *     tags: [Province]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The unique identifier of the province.
+ *     responses:
+ *       200:
+ *         description: Province found successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: number
+ *                   example: 200
+ *                   description: The HTTP status code indicating success.
+ *                 status:
+ *                   type: string
+ *                   example: success
+ *                   description: The status of the response.
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                   description: A brief message describing the result.
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       example: 1
+ *                       description: The unique identifier of the province.
+ *                     name:
+ *                       type: string
+ *                       example: Jakarta
+ *                       description: The name of the province.
+ *       404:
+ *         description: Province not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: number
+ *                   example: 404
+ *                   description: The HTTP status code indicating that the province was not found.
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                   description: The status of the response indicating an error.
+ *                 message:
+ *                   type: string
+ *                   example: Province not found
+ *                   description: A brief message describing the error.
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: number
+ *                   example: 500
+ *                   description: The HTTP status code indicating an internal server error.
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                   description: The status of the response indicating an error.
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ *                   description: A brief message describing the error.
+ */
 // Get a province by ID
 router.get('/api/v1/province/:id', getProvinceById);
 
